65ef6b5b7348ab04c60339ad5f64cb57dce4ce1a
Chris Middleton <ctjmiddleton@gmail.com>
2022-02-23 14:20:37 +0000

    Price feature complete - tests pass

--- spec/features/add_space_spec.rb
+++ spec/features/add_space_spec.rb
@@ -14,27 +14,4 @@ feature "add a space" do
   end
 end
 
-  # scenario "User submits description of space" do 
-  #   visit('/spaces/new')
-
-  #   fill_in('title', with: 'Nice bedroom')
-  #   fill_in('description', with: 'This is a really but really really nice bedroom')
-  #   fill_in('price', with: 50.00)
-  #   click_button('Submit')
-
-  #   expect(page).to have_content 'This is a really but really really nice bedroom'
-  # end 
-
-  # scenario "User submits price of space" do 
-  #   visit('/spaces/new')
-
-  #   fill_in('title', with: 'Nice bedroom')
-  #   fill_in('description', with: 'This is a really but really really nice bedroom')
-  #   fill_in('price', with: 50.00)
-  #   click_button('Submit')
-
-  #   expect(page).to have_content '£50 per night'
-  # end 
-
-
-
+  
